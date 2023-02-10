53ed4b12f00658939e802e79673e503ec8d7ab2a
Faustino Loeza <faustinoloezaperez@gmail.com>
2023-02-10 05:40:39 +0000

    location message

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart';
 
 void main() {
   runApp(MyApp());
+}
+
+void myLog(context) async {
+  Location location = new Location();
+  //location.enableBackgroundMode(enable: true);
+  //location.enableBackgroundMode(enable: true);
+
+  bool _serviceEnabled;
+  PermissionStatus _permissionGranted;
+  LocationData _locationData;
+
+  _locationData = await location.getLocation();
+  //return _locationData;
+  print("____________________");
+  print('$_locationData.latitude, $_locationData.longitude');
+  print("OK");
+  showAlertDialog(context, _locationData.latitude, _locationData.longitude);
 }
 
 class MyApp extends StatelessWidget {
@@ -37,36 +55,77 @@ class MyHomePage extends StatelessWidget {
             Container(
               padding: const EdgeInsets.all(8),
               child: Center(child: Text(title)),
-              color: Colors.orange[200],
+              color: Colors.orange[400],
             ),
             Container(
               padding: const EdgeInsets.all(8),
               child: Center(child: Text(title)),
-              color: Colors.green[200],
+              color: Colors.green[400],
             ),
             Container(
               padding: const EdgeInsets.all(8),
               child: Center(child: Text(title)),
-              color: Colors.red[200],
+              color: Colors.red[400],
             ),
             Container(
               padding: const EdgeInsets.all(8),
               child: Center(child: Text(title)),
-              color: Colors.purple[200],
+              color: Colors.purple[400],
             ),
             Container(
               padding: const EdgeInsets.all(8),
               child: Center(child: Text(title)),
-              color: Colors.blueGrey[200],
+              color: Colors.blueGrey[400],
             ),
             Container(
               padding: const EdgeInsets.all(8),
               child: Center(child: Text(title)),
-              color: Colors.yellow[200],
+              color: Colors.yellow[400],
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              color: Colors.blueAccent,
+              child: TextButton(
+                onPressed: () => {myLog(context)},
+                child: const Text(
+                  "Login",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w300,
+                      fontStyle: FontStyle.italic,
+                      fontFamily: 'Open Sans',
+                      fontSize: 24),
+                ),
+              ),
             ),
           ],
         ),
       ),
     );
   }
+}
+
+void showAlertDialog(BuildContext context, dynamic latitud, dynamic longitud) {
+  // set up the button
+  Widget okButton = TextButton(
+    child: Text("OK"),
+    onPressed: () {},
+  );
+
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: Text("My title"),
+    content: Text('$latitud, $longitud'),
+    actions: [
+      okButton,
+    ],
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
 }
